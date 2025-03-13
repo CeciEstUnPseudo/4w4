8bb8ddd6f1e1bb9d2b8bb7ac8b6c62acbc8ee4ba
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-03-13 11:21:14 -0400

    Customizer couleur + background-image

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
 <footer>
 
+<style>
+        .hero_couleur{
+            color: <?php echo $hero_text_color ?>;
+        }
 
+    </style>
+
+<?php $hero_text_color = get_theme_mod('hero_couleur', ''); ?>
 <?php $footer_mission = get_theme_mod("footer_mission", "Default" ) ?>
 <?php $footer_adresse = get_theme_mod("footer_adresse", "Default" ) ?>
 <?php $footer_lieu = get_theme_mod("footer_lieu", "Default" ) ?>
@@ -9,7 +16,7 @@
     <div class="piedpage global">
         <section class="piedpage__s1">
             <div class="piedpage__s1__externe">
-                <p class="piedpage__s1__externe__titre">Liste de Liens</p>
+                <p class="piedpage__s1__externe__titre hero_couleur">Liste de Liens</p>
                 <?php wp_nav_menu(array(
                     "menu" => "externe",
                     "container" => "nav",
@@ -17,7 +24,7 @@
                 )); ?>
             </div>
             <div class="piedpage__s1__externe liste2">
-                <p class="piedpage__s1__externe__titre">Nos categories</p>
+                <p class="piedpage__s1__externe__titre hero_couleur">Nos categories</p>
                 <?php wp_nav_menu(array(
                     "menu" => "principal",
                     "container" => "nav",
@@ -25,7 +32,7 @@
                 )); ?>
             </div>
             <div class="piedpage__s1__adresse">
-                <div class="piedpage__s1__adresse__coord">
+                <div class="piedpage__s1__adresse__coord hero_couleur">
                 <?php echo $footer_adresse ?> | <?php echo $footer_lieu ?> | <?php echo $footer_telephone ?>
                 </div>
                 <div class="piedpage__s1__adresse__recherche">
@@ -37,8 +44,8 @@
                 <img class="hero__icones" src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32">
             </div>
             </div>
-            <div class="piedpage__s1__description">
-                <p class="piedpage__s1__description__titre">Qui sommes-nous?</p>
+            <div class="piedpage__s1__description hero_couleur">
+                <p class="piedpage__s1__description__titre hero_couleur">Qui sommes-nous?</p>
                 <?php echo $footer_mission ?>
             </div>
         </section>
--- front-page.php
+++ front-page.php
@@ -6,39 +6,49 @@
 
     <?php $hero_auteur = get_theme_mod('hero_auteur', ''); ?>
     <?php $hero_background = get_theme_mod('hero_background', ''); ?>
-    <?php $hero_text_color = get_theme_mod('hero_text_color', ''); ?>
+    <?php $hero_text_color = get_theme_mod('hero_couleur', ''); ?>
     <?php $hero_email = get_theme_mod('hero_mail', ''); ?>
     <?php $hero_lieu = get_theme_mod('hero_lieu', ''); ?>
     <?php $hero_telephone = get_theme_mod('hero_telephone', ''); ?>
 
-    <section class="hero" style="color: <?php echo $hero_text_color ?> background-image: url('<?php echo $hero_background; ?>')">
+    <style>
+        .hero_couleur{
+            color: <?php echo $hero_text_color ?>;
+        }
+
+    </style>
+    <section class="hero" 
+    style="
+    background-image: url('<?php echo $hero_background; ?>')
+    ">
+
         <div class="hero__contenu">
-            <h1 class="hero__titre">
+            <h1 class="hero__titre hero_couleur">
                 <?php echo bloginfo("name"); ?>
                 <!-- En haut va prendre la valeur de "Site Title" des settings -->
             </h1>
-            <p class="hero__description"> <?php bloginfo("description") ?>
+            <p class="hero__description hero_couleur"> <?php bloginfo("description") ?>
 
             </p>
 
             <?php  ?>
 
-            <p class="hero__addresse-electronique"><?php echo $hero_email ?></p>
-            <p class="hero__addresse-physique"><?php echo $hero_lieu ?></p>
-            <p class="hero__telephone"><?php echo $hero_telephone ?></p>
+            <p class="hero__addresse-electronique hero_couleur"><?php echo $hero_email ?></p>
+            <p class="hero__addresse-physique hero_couleur"><?php echo $hero_lieu ?></p>
+            <p class="hero__telephone hero_couleur"><?php echo $hero_telephone ?></p>
             <a href="" class="hero__courriel"></a>
             <button class="hero__bouton">S'INSCRIRE</button>
             <div class="hero__icones-box">
                 <img class="hero__icones" src="https://s2.svgbox.net/social.svg?ic=facebook&color=000" width="32" height="32">
                 <img class="hero__icones" src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32">
             </div>
-            <p>Auteur: <?php echo $hero_auteur; ?></p>
+            <p class="hero_couleur">Auteur: <?php echo $hero_auteur; ?></p>
         </div>
     </section>
 
     <section class="formulaire">
         <div class="formulaire__section">
-            <p class="formulaire__section--titre">
+            <p class="formulaire__section--titre hero_couleur">
                 Nom
             </p>
             <form action="" class="formulaire__section--form">
@@ -46,7 +56,7 @@
             </form>
         </div>
         <div class="formulaire__section">
-            <p class="formulaire__section--titre">
+            <p class="formulaire__section--titre hero_couleur">
                 Prénom
             </p>
             <form action="" class="formulaire__section--form">
@@ -54,7 +64,7 @@
             </form>
         </div>
         <div class="formulaire__section">
-            <p class="formulaire__section--titre">
+            <p class="formulaire__section--titre hero_couleur">
                 Courriel
             </p>
             <form action="" class="formulaire__section--form">
@@ -62,7 +72,7 @@
             </form>
         </div>
         <div class="formulaire__section">
-            <p class="formulaire__section--titre">
+            <p class="formulaire__section--titre hero_couleur">
                 Téléphone
             </p>
             <form action="" class="formulaire__section--form">
@@ -70,11 +80,11 @@
             </form>
         </div>
 
-        <button class="formulaire__bouton">S'INSCRIRE</button>
+        <button class="formulaire__bouton hero_couleur">S'INSCRIRE</button>
     </section>
 
     <section class="galerie">
-        <h2 class="galerie__titre">Nos destinations favorites</h2>
+        <h2 class="galerie__titre hero_couleur">Nos destinations favorites</h2>
 
         <?php if (in_category("galerie")) { ?>
             <figure class="galerie__figure">
