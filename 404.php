bf18e6779068c87cc64f5aad72dbe6676bae0748
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-03-25 11:44:15 -0400

    Remplacement du bouton pour un menu WHC + Ajouts liens pour logos

--- 404.php
+++ 404.php
@@ -8,7 +8,11 @@
     <?php $erreur_logo1 = get_theme_mod('erreur_logo1', ''); ?>
     <?php $erreur_logo2 = get_theme_mod('erreur_logo2', ''); ?>
     <?php $erreur_logo3 = get_theme_mod('erreur_logo3', ''); ?>
-    <?php $erreur_bouton_texte = get_theme_mod("erreur_bouton_texte", "") ?>
+
+    <?php $erreur_logo1_lien = get_theme_mod('erreur_logo1_lien', ''); ?>
+    <?php $erreur_logo2_lien = get_theme_mod('erreur_logo2_lien', ''); ?>
+    <?php $erreur_logo3_lien = get_theme_mod('erreur_logo3_lien', ''); ?>
+    <?php $erreur_menu_titre = get_theme_mod("erreur_menu_titre", "") ?>
 
     <?php $erreur_couleur_texte = get_theme_mod('erreur_couleur_texte', ''); ?>
 
@@ -22,14 +26,29 @@
         <section class="erreur erreur_couleur">
           <h1 class="erreur__404"><?php echo $erreur_404 ?></h1>
           <p class="erreur__description"><?php echo $erreur_description ?></p>
-          <div class="erreur__bouton--contenant">
-            <button class="erreur__bouton--retour erreur_couleur" ><?php echo $erreur_bouton_texte ?></button>
+
+          <p class="erreur__nav__titre"><?php echo $erreur_menu_titre ?></p>
+          <div class="erreur__nav">
+            <?php wp_nav_menu(array(
+                        'menu'                 => '404',
+                        'container'            => 'nav',
+                        'container_class'      => 'erreur__menu',
+            ));
+          ?>
+
 
           </div>
           <div class="erreur__logos">
-            <img src="<?php echo $erreur_logo1 ?>" alt="logo1" class="erreur__logos__logo--1">
-            <img src="<?php echo $erreur_logo2 ?>" alt="logo2" class="erreur__logos__logo--2">
-            <img src="<?php echo $erreur_logo3 ?>" alt="logo3" class="erreur__logos__logo--3">
+            <!-- Chaque balise est dans un a href pour que, une fois cliqué, nous soyons amener à la page web -->
+            <a href="<?php echo $erreur_logo1_lien ?>">
+              <img src="<?php echo $erreur_logo1 ?>" alt="logo3" class="erreur__logos__logo--3">
+            </a>
+            <a href="<?php echo $erreur_logo2_lien ?>">
+              <img src="<?php echo $erreur_logo2 ?>" alt="logo3" class="erreur__logos__logo--3">
+            </a>
+            <a href="<?php echo $erreur_logo3_lien ?>">
+              <img src="<?php echo $erreur_logo3 ?>" alt="logo3" class="erreur__logos__logo--3">
+            </a>
           </div>
         </section>
 
