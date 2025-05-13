5711f2db39fda7b2435c6a28a57d89e283e81b42
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2025-05-13 14:04:29 -0400

    Ajouts des infos supplémentaires à la page des destinations (auteur, date, liste catégories)

--- single.php
+++ single.php
@@ -2,6 +2,9 @@
 // Modèle single.php est le modèle pour une destination en particulier
 ?>
 
+<?php $single_auteur = get_theme_mod('hero_auteur', ''); ?>
+<?php $date_publication = get_the_date("j F Y à H:i"); ?>
+
 
 <?php get_header() ?>
         <section class="single__contenu">
@@ -25,6 +28,24 @@
                         <p>Temp. Min: <?php echo the_field("temperature_minimum"); ?> °C</p>
                         <p>Temp. Max: <?php echo the_field("temperature_maximum"); ?> °C</p>
                 </div>
+
+                <div class="single__informations">
+                        <p class="single__auteur">Auteur: <?php echo $single_auteur; ?></p>
+                        <p class="single__publication"> <?php echo $date_publication; ?></p>
+                        <!-- Liste des catégories de cette page -->
+                        
+                        <p class="single__categories">Catégories:
+                                <?php
+                                $categories = get_the_category();
+                                if ($categories) {
+                                        foreach ($categories as $categorie) {
+                                                echo '<a href="' . esc_url(get_category_link($categorie->term_id)) . '">' . esc_html($categorie->name) . '</a> ';
+                                        }
+                                }
+                                ?>
+                        </p>
+                </div>
+
         </div>
         </section>
 
